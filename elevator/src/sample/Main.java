f2f77a51cf601ce3e668788237c083151c551b92
Gloria Sara Panturu <31956990+gspanturu@users.noreply.github.com>
2019-02-03 20:27:29 +0100

    newest Version

--- elevator/src/sample/Main.java
+++ elevator/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Elevator - Readers Writers Problem");
-        primaryStage.setScene(new Scene(root, 700, 475));
+        primaryStage.setScene(new Scene(root, 800, 475));
         primaryStage.show();
     }
 
